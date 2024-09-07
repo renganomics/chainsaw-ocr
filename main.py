b1f12a43af4a3915a16f6485ba9a5ab9d848c7dc
Oreng <orengasi@gmail.com>
2024-09-07 18:53:28 +0100

    Created progress bar and cleaned up output

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
-import PIL
 import pytesseract
-from threading import Thread
 
 from PIL import Image
 from pathlib import Path
 from progress_bar import progress_bar
 
 filepath = Path("C:/Users/oreng/Pictures")
+
+results = []
 
 file_counter = 0
 for file in os.listdir(filepath):
@@ -16,29 +16,12 @@ for file in os.listdir(filepath):
 progress_bar(0, file_counter)
 
 for index, file in enumerate(filepath.glob("*")):
-    print(f"{file}")
-
-    # folder_path = Path(f"C:/Users/oreng/Pictures/{file}").glob("*")
-    # for folder_file in folder_path:
-    #     try:
-    #         print(pytesseract.image_to_string(Image.open(file)))
-    #     except (PermissionError, PIL.UnidentifiedImageError):
-    #         if PermissionError:
-    #             print(f"There was a PermissionError for {file}.")
-    #         if PIL.UnidentifiedImageError:
-    #             print(f"There was an UnidentifiedImageError for {file}.\n")
-    #     continue
-
     try:
         text = pytesseract.image_to_string(Image.open(file))
-        print(text)
-        progress_bar(index + 1, file_counter)
-
-    except (PermissionError, PIL.UnidentifiedImageError):
-        if PermissionError:
-            print(f"There was a PermissionError for {file}.")
-        if PIL.UnidentifiedImageError:
-            print(f"There was an UnidentifiedImageError for {file}.\n")
+        if text:
+            results.append(f"{file}:\n{text}")
+    except (PermissionError, Image.UnidentifiedImageError):
         continue
+    progress_bar(index, file_counter)
 
-
+[print(f"\n{result}") for result in results]
--- progress_bar.py
+++ progress_bar.py
@@ -1,8 +1,17 @@
+import math
+
 def progress_bar(progress, total):
     percent = 100 * (progress/float(total))
     bar = "█" * int(percent) + "-" * (100 - int(percent))
-    print(f"\r|{bar}| {percent:2f}%", end="\r")
+    print(f"\r|{bar}| {percent:.2f}%", end="\r")
+
+
 
 if __name__ == "__main__":
-    for count in range(1000, 20000):
-        progress_bar(count, 20000)
+    numbers = [x * 5 for x in range(2000, 3000)]
+    results = []
+
+    progress_bar(0, len(numbers))
+    for i, x in enumerate(numbers):
+        results.append(math.factorial(x))
+        progress_bar(i, len(numbers))
