93e698ad148ae1609f343f13e503a08d58289cc0
Oreng <orengasi@gmail.com>
2024-09-09 12:58:02 +0100

    Merged progress and image_reader methods and added general comments

--- main.py
+++ main.py
@@ -11,25 +11,38 @@ class Reader:
         self.filepath = filepath
         self.results = []
 
-    def progress(self, count=0, file_counter=0):
-        for self.file in os.listdir(self.filepath):
-            file_counter = file_counter + 1
-        return progress_bar(count, file_counter)
-
     def image_reader(self):
-        for index, file in enumerate(self.filepath.glob("*")):
-            try:
-                text = pytesseract.image_to_string(Image.open(file))
-                if text:
-                    self.results.append(f"{file}:\n{text}")
-            except (PermissionError, Image.UnidentifiedImageError):
-                continue
-            self.progress(index)
-        [print(f"\n{result}") for result in self.results]
+        """Loads progress bar while reading through permitted images in a
+        directory"""
+        try:
+            # Get total file count of directory
+            file_counter = 0
+            for self.file in os.listdir(self.filepath):
+                file_counter = file_counter + 1
+
+            # Load progress bar with the loop index and total file count
+            progress_bar(0, file_counter)
+            for index, file in enumerate(self.filepath.glob("*")):
+                progress_bar(index, file_counter)
+
+                # Parse through directory for image files and scan for text
+                try:
+                    text = pytesseract.image_to_string(Image.open(file))
+                    if text:
+                        self.results.append(f"{file}:\n{text}")
+
+                except (PermissionError, Image.UnidentifiedImageError):
+                    continue
+
+            [print(f"\n{result}") for result in self.results]
+
+        except (FileNotFoundError, NotADirectoryError):
+            print("This directory does not exist.")
 
 
 if __name__ == "__main__":
-    pic_path = Path("C:/Users/oreng/Pictures")
+    # prompt = input("Please enter a directory: ")
+    test_path = "C:/Users/oreng/Pictures/"
+    pic_path = Path(test_path)
     reader = Reader(filepath=pic_path)
-    reader.progress()
     reader.image_reader()
