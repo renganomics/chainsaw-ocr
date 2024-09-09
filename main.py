a59ef51af9ca02544245c8439cdcb04a428dfbe1
Oreng <orengasi@gmail.com>
2024-09-09 02:36:35 +0100

    Created Reader class to house progress bar and image_reader methods and tested simple filtering on API

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ from PIL import Image
 from pathlib import Path
 from progress_bar import progress_bar
 
-filepath = Path("C:/Users/oreng/Pictures")
+pic_path = Path("C:/Users/oreng/Pictures")
 
-results = []
+class Reader:
 
-file_counter = 0
-for file in os.listdir(filepath):
-    file_counter = file_counter + 1
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.results = []
 
-progress_bar(0, file_counter)
+    def progress(self, count=0, file_counter=0):
+        for self.file in os.listdir(self.filepath):
+            file_counter = file_counter + 1
+        return progress_bar(count, file_counter)
 
-for index, file in enumerate(filepath.glob("*")):
-    try:
-        text = pytesseract.image_to_string(Image.open(file))
-        if text:
-            results.append(f"{file}:\n{text}")
-    except (PermissionError, Image.UnidentifiedImageError):
-        continue
-    progress_bar(index, file_counter)
+    def image_reader(self):
+        for index, file in enumerate(self.filepath.glob("*")):
+            try:
+                text = pytesseract.image_to_string(Image.open(file))
+                if text:
+                    self.results.append(f"{file}:\n{text}")
+            except (PermissionError, Image.UnidentifiedImageError):
+                continue
+            self.progress(index)
+        [print(f"\n{result}") for result in self.results]
 
-[print(f"\n{result}") for result in results]
+
+if __name__ == "__main__":
+    reader = Reader(filepath=pic_path)
+    reader.progress()
+    reader.image_reader()
+
