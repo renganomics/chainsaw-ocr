7e27f4e95106dcf2910068b2f213e3f685c97372
Oreng <orengasi@gmail.com>
2024-09-09 13:02:46 +0100

    Merged progress and image_reader methods and added general comments

--- progress_bar.py
+++ progress_bar.py
@@ -2,7 +2,10 @@ import math
 
 
 def progress_bar(progress, total):
+    """To be used within a for loop to keep track of progress over iterations"""
     percent = 100 * (progress/float(total))
+    # Connect "█" to percentage to overtake "-" and display progress percentage
+    # to two decimal places
     bar = "█" * int(percent) + "-" * (100 - int(percent))
     print(f"\r|{bar}| {percent:.2f}%", end="\r")
 
