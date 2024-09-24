04bc54dbb24b1296f69b8709fbfda7184365c5ff
Oreng <orengasi@gmail.com>
2024-09-24 19:47:14 +0100

    Populated get_page_metadata method of MangaDexRequests class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 
+BASE_URL = "https://api.mangadex.org"
+
 
 class MangaDexRequests:
 
@@ -8,30 +10,41 @@ class MangaDexRequests:
 
     def get_manga_data(self, title, language):
 
-        base_url = "https://api.mangadex.org"
-
         mangas = requests.get(
-            f"{base_url}/manga",
+            f"{BASE_URL}/manga",
             params={"title": title}
         )
         manga_id = [manga["id"] for manga in mangas.json()["data"]][0]
 
         chapters = requests.get(
-            f"{base_url}/manga/{manga_id}/feed",
+            f"{BASE_URL}/manga/{manga_id}/feed",
             params={"translatedLanguage[]": language,
                     "order[chapter]": "asc"}
         )
         # print(chapters.json())
-        chapter_ids = [chapter["id"] for chapter in chapters.json()["data"] if
-                       chapter["attributes"]["externalUrl"] is None]
+
+        chapter_ids = [chapter["id"] for chapter in chapters.json()["data"]
+                       if chapter["attributes"]["externalUrl"] is None]
         attributes = [chapter["attributes"] for chapter in
                       chapters.json()["data"] if chapter["attributes"]
                       ["externalUrl"] is None]
         self.chapter_data = {"id": chapter_ids, "attributes": attributes}
         return self.chapter_data
 
-    def get_page_metadata(self, chapter_id):
-        pass
+    @staticmethod
+    def get_page_metadata(chapter_id):
+
+        metadata = requests.get(
+            f"{BASE_URL}/at-home/server/{chapter_id}"
+        )
+        # print(metadata.json())
+        base_url = metadata.json()["baseUrl"]
+        chapter_hash = metadata.json()["chapter"]["hash"]
+        chapter_data = metadata.json()["chapter"]["data"]
+
+        page_links = [f"{base_url}/data/{chapter_hash}/{page}" for page in
+                      chapter_data]
+        return page_links
 
     def download_url(self, image_url, filepath):
         pass
@@ -75,4 +88,5 @@ class ImageReader:
 
 if __name__ == "__main__":
     mdx = MangaDexRequests()
-    print(mdx.get_manga_data("chainsaw man", "en"))
+    mdx.get_manga_data("chainsaw man", "en")
+    print(mdx.get_page_metadata("73af4d8d-1532-4a72-b1b9-8f4e5cd295c9"))
